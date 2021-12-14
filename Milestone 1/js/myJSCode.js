deacb666ee42a778e5e1d0952d85e2a4d0f5c1f3
Elale1984 <69740507+Elale1984@users.noreply.github.com>
2021-12-13 20:09:28 -0600

    added Javascript to project

--- Milestone 1/js/myJSCode.js
+++ Milestone 1/js/myJSCode.js
@@ -14,4 +14,50 @@ window.addEventListener('scroll', ()=> {
     else{
         content.remove.classList('active');
     }
+
 });
+
+// This function will hide the image of my family upon loading
+function onLoadFirstPage(){
+    document.getElementById("mySlideImage").style.visibility = 'hidden';
+}
+
+// This function controls the fade in upon scrolling down
+function loadFadeIn(){
+    let content = document.querySelector('.row2');
+    content.classList.add('active');
+    
+}
+
+
+// This function uses switch statement to control a clickable text witch at first shows the first image
+// and then next changes the image. The text changes with the image as well
+
+var imgClickCount = 0;
+
+function onClickText(){
+    document.getElementById("mySlideImage").style.visibility = 'visible';
+
+    // if statement is for resetting the counter so that the process starts over
+    if(imgClickCount == 1){
+        imgClickCount = 0;
+    }
+    else {
+        imgClickCount++; // else we increase the count 
+    }
+    
+    // switch to set the src of the image and set the appropriate text
+    switch (imgClickCount){
+
+        case 0: document.getElementById("mySlideImage").src = "/Milestone 1/images/mygirls.jpg";
+                document.getElementById("clickPicText").innerText = "My Daughters, Izabella and Natalee"
+            break;
+        case 1: document.getElementById("mySlideImage").src = "/Milestone 1/images/wife.jpg";
+        document.getElementById("clickPicText").innerText = "Me and my lovely wife, Nicole"
+            break;
+        
+    }
+}
+
+
+
